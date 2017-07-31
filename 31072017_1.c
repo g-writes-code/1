b7528066fa9fbd45c09f627487fab2b81f2bd5c9
g-writes-code <30595208+g-writes-code@users.noreply.github.com>
2017-08-01 00:03:46 +1200

    Create 31072017_1.c

---
+++ 31072017_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#error NIGGER
+int main() { return 1; }
+int main (){
+print("Hello, world!")
+define true false; 
+}
+; return 0}
+<html>
+` `
+#include <efi.h>
+(defun myopinion (option) (write-line "nice job gentlemens you now have a retarded code) (write-line option))
+(myopinion "saged")
+/*
+GOTO 10
+--this is a comment
+*/
